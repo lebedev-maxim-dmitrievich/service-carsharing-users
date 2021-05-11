841ebe5e5120c1d60e0a6fc646b2cf30ea4602c3
maxim-lebedev <cyue.alle@gmail.com>
2021-05-11 21:18:05 +0300

    добавил DTO для сущности пользователя

---
+++ src/main/java/ru/lebedev/servicecarsharingusers/dto/UserDTO.java
@@ -0,0 +1,77 @@
+package ru.lebedev.servicecarsharingusers.dto;
+
+public class UserDTO {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String patronymic;
+    private int age;
+    private int passportID;
+    private int drivingLicenceID;
+    private String photoUserBase64;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getPassportID() {
+        return passportID;
+    }
+
+    public void setPassportID(int passportID) {
+        this.passportID = passportID;
+    }
+
+    public int getDrivingLicenceID() {
+        return drivingLicenceID;
+    }
+
+    public void setDrivingLicenceID(int drivingLicenceID) {
+        this.drivingLicenceID = drivingLicenceID;
+    }
+
+    public String getPhotoUserBase64() {
+        return photoUserBase64;
+    }
+
+    public void setPhotoUserBase64(String photoUserBase64) {
+        this.photoUserBase64 = photoUserBase64;
+    }
+}
