c29bcf3a0f0e7f0720d6c48364fc600f439a036c
maxim-lebedev <cyue.alle@gmail.com>
2021-05-11 20:56:11 +0300

    init commit

---
+++ src/main/java/ru/lebedev/servicecarsharingusers/ServiceCarsharingUsersApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedev.servicecarsharingusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCarsharingUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCarsharingUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/lebedev/servicecarsharingusers/ServiceCarsharingUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebedev.servicecarsharingusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCarsharingUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
